20ce6f51bccabce2ad27c8d1348900d3ac858222
Trotyl <trotyl@qq.com>
2018-01-21 15:47:32 +0800

    [evanswu&SunDi] - finish the case case3;

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,3 +1,20 @@
 public class MarsRover {
 
+    public MarsRover(int positionX, int positionY, String direction) {
+
+    }
+
+    public String run(String command) {
+        if (command.equals("L")) {
+            return "(0, 0, W)";
+        } else if(command.equals("R")){
+            return "(0, 0, E)";
+        } else if(command.equals("M")){
+            return "(0, 1, N)";
+        } else{
+            return null;
+        }
+    }
+
+
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -6,7 +6,29 @@ import static org.junit.Assert.assertThat;
 public class MarsRoverTest {
 
     @Test
-    public void hello_world() throws Exception {
-        assertThat(1 + 1, is(2));
+    public void should_turn_west_after_receiving_L() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        String result = marsRover.run("L");
+
+        assertThat(result, is("(0, 0, W)"));
+    }
+
+    @Test
+    public void should_turn_east_after_receiving_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        String result = marsRover.run("R");
+
+        assertThat(result, is("(0, 0, E)"));
+    }
+
+    @Test
+    public void should_move_to_0_1_N_after_receiving_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        String result = marsRover.run("M");
+
+        assertThat(result, is("(0, 1, N)"));
     }
 }
